fe473ff87145c4588b164c93db6d5cc5ce5cdab3
Gleb Potapov <glebpotapov2001@gmail.com>
2020-09-18 22:20:45 +0300

    Code refactors. Implement global tags changong.

--- index.js
+++ index.js
@@ -12,47 +12,52 @@ const prefix = "!";
 
 let interval = null;
 
-const tags = "azur_lane";
+let seconds = 900;
 
-const channelId = "755514265355681879";
-
-client.on("ready", () => {
-  interval = setInterval(() => {
-    sendPost(tags);
-  }, 900 * 1000);
-});
-
-const sendPost = (tagsString) => {
+const sendPost = (tagsString, channel) => {
   booru
     .posts({ tags: tagsString, limit: 1, random: true })
     .then((posts) => {
       const img = posts[0];
-      const channel = client.channels.get(channelId);
       const attachment = new Discord.Attachment(img.large_file_url);
       channel.send(img.tag_string_character, attachment);
     })
     .catch((error) => {
-      const channel = client.channels.get(channelId);
-      channel.send("Что-то пошло не так");
+      console.log(error);
+      channel.send(`Произошла ошибка ${error.code}`);
     });
-};
-
-const changeGlobalTags = (newTags) => {
-  tags = newTags;
 };
 
 client.on("message", (msg) => {
   if (!msg.content.startsWith(prefix)) return;
   const args = msg.content.slice(prefix.length).trim().split(" ");
   const command = args.shift().toLowerCase();
+  const currentChannel = msg.channel;
+  if (command === "start") {
+    const tags = "azur_lane";
+    currentChannel.send(`Начинаю с тегами ${tags}`);
+    interval = setInterval(() => {
+      sendPost(tags, currentChannel);
+    }, seconds * 1000);
+  }
+
+  if (command === "change") {
+    const tags = args.join(" ");
+    currentChannel.send(`Теги изменены на ${tags}`);
+    clearInterval(interval);
+    interval = setInterval(() => {
+      sendPost(tags, currentChannel);
+    }, seconds * 1000);
+  }
+
   if (command === "interval") {
     clearInterval(interval);
     interal = setInterval(() => {
-      sendPost(tags);
+      sendPost(tags, currentChannel);
     }, args[1] * 1000);
   }
   if (command === "send") {
-    sendPost(args.join(" "));
+    sendPost(args.join(" "), currentChannel);
   }
 });
 
